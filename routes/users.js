6bfde7225e90fb22ca7791756a650795f53e3193
Isaias Neto <isaiasneto@gmail.com>
2019-08-21 00:39:39 -0300

    add env_default and users select frm DB

--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,17 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+
+	const { Client } = require('pg')
+	const client = new Client()
+	client.connect()
+	client
+	.query('SELECT * FROM users')
+	.then(resdb => {
+		// console.log(resdb.rows[0]);
+		res.send(resdb.rows);
+	})
+	.catch(e => console.error(e.stack))
 });
 
 module.exports = router;
